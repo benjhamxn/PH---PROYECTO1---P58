b4d6c4be099711b0bf018c3de65764f6491e6f25
Benjamin Rios A <65048010+benjhamxn@users.noreply.github.com>
2021-05-18 18:30:29 -0500

    Add files via upload

---
+++ Empresa X/js/script.js
@@ -0,0 +1,13 @@
+//Variables
+var nav = `<ul>
+<li><a href="login.html">Iniciar Sesion</a></li>
+<li><a href="somos.html">Somos</a></li>
+<li><a href="servicios.html">Servicios</a></li>
+<li><a href="proyectos.html">Proyectos</a></li>
+<li><a href="blog.html">Blog</a></li>
+<li><a href="contactanos.html">Contactanos</a></li>
+</ul>`;
+
+
+//Llamadas
+document.getElementById("navegacion").innerHTML = nav;
